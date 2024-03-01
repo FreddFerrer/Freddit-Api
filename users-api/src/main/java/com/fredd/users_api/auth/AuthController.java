2aede01579d1acdc7741de03957f61fe20817e36
Freddy <ferreirafreddy39@gmail.com>
2024-02-29 22:49:01 -0300

    login added

--- users-api/src/main/java/com/fredd/users_api/auth/AuthController.java
+++ users-api/src/main/java/com/fredd/users_api/auth/AuthController.java
@@ -1,4 +1,32 @@
 package com.fredd.users_api.auth;
 
+import com.fredd.users_api.model.dtos.LoginResponseDto;
+import com.fredd.users_api.model.dtos.request.CreateUserDto;
+import com.fredd.users_api.model.dtos.request.LoginRequestDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDto> login(@RequestBody @Valid LoginRequestDto request)
+    {
+        return ResponseEntity.ok(authService.login(request));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<LoginResponseDto> register(@RequestBody @Valid CreateUserDto request)
+    {
+        return ResponseEntity.ok(authService.register(request));
+    }
 }
